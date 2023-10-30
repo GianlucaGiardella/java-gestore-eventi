595ed634cea32c73ecb54380612307f242af356c
GianluGiarde <gianlugiarde@gmail.com>
2023-10-30 17:07:12 +0100

    add milestone 4

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -5,11 +5,88 @@ import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // Prova della classe ProgrammEventi
+
+        System.out.println("\n- Nuovo programma");
+
+        try {
+
+            ProgrammEventi programmEventi = new ProgrammEventi("Programma di prova");
+
+            programmEventi.aggiungiEvento(new Evento(
+                    "Evento1",
+                    LocalDate.of(3000, 10, 30),
+                    100));
+
+            programmEventi.aggiungiEvento(new Evento(
+                    "Evento2",
+                    LocalDate.of(2027, 11, 1),
+                    200));
+
+            programmEventi.aggiungiEvento(new Evento(
+                    "Evento3",
+                    LocalDate.of(2030, 10, 4),
+                    300));
+
+            programmEventi.aggiungiEvento(new Evento(
+                    "Evento4",
+                    LocalDate.of(2023, 12, 31),
+                    400));
+
+            programmEventi.aggiungiEvento(new Evento(
+                    "Evento5",
+                    LocalDate.of(2023, 12, 31),
+                    500));
+
+            System.out.println("\nProgramma eventi:" + "\n"
+                    + programmEventi.toString());
+
+            System.out.println("\nEventi trovati: " + programmEventi
+                    .cercaEventiDallaData(LocalDate.of(2023, 12, 31)));
+
+            System.out.println("\nNumero di eventi nel programma: " + programmEventi.numeroEventi());
+
+            System.out.println("\n" + programmEventi.eventiOrdinatiPerData());
+
+            programmEventi.svuotaEventi();
+
+            System.out.println("\nProgramma eventi svuotato:" + "\n"
+                    + programmEventi.toString());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("\n- Errore: " + e.getMessage());
+        }
+
+        System.out.println("\n==============================");
+
+        // Prova della classe Concerto
+
+        System.out.println("\n- Nuovo concerto");
+
+        try {
+            Concerto concerto = new Concerto(
+                    "Concerto di prova",
+                    LocalDate.of(2030, 12, 31),
+                    100,
+                    LocalTime.of(23, 59, 59),
+                    new BigDecimal("10.99")
+            );
+
+            System.out.println("\n- Concerto creato con successo !");
+
+            System.out.println(concerto);
+        } catch (IllegalArgumentException e) {
+            System.out.println("\n- Errore: " + e.getMessage());
+        }
+
+        System.out.println("\n==============================");
 
         boolean esci = false;
 
@@ -81,6 +158,8 @@ public class Main {
                         }
                     }
 
+                } catch (NumberFormatException e) {
+                    System.out.println("\n- Errore: Numero non valido");
                 } catch (IllegalArgumentException e) {
                     System.out.println("\n- Errore: " + e.getMessage());
                 }
@@ -88,43 +167,6 @@ public class Main {
                 System.out.println("\n- Buona giornata");
             }
         }
-
-        // Prova della classe Concerto
-
-        /*while (!esci) {
-            System.out.println("\n- Vuoi aggiungere un nuovo concerto ? y | n");
-            esci = scanner.nextLine().equals("n");
-
-            if (!esci) {
-                System.out.println("\n- Nuovo concerto");
-
-                try {
-                    System.out.println("Inserire il titolo: ");
-                    String titolo = scanner.nextLine();
-
-                    System.out.println("Inserire la data: ");
-                    LocalDate data = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
-                    System.out.println("Inserire l'ora: ");
-                    LocalTime ora = LocalTime.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("HH:mm:ss"));
-
-                    System.out.println("Inserire il numero di posti totali: ");
-                    int postiTotali = Integer.parseInt(scanner.nextLine());
-
-                    System.out.println("Inserire il prezzo: ");
-                    BigDecimal prezzo = new BigDecimal(scanner.nextLine());
-
-                    Concerto concerto = new Concerto(titolo, data, postiTotali, ora, prezzo);
-                    System.out.println("\n- Concerto creato con successo !");
-
-                    System.out.println(concerto);
-                } catch (IllegalArgumentException e) {
-                    System.out.println("\n- Errore: " + e.getMessage());
-                }
-            } else {
-                System.out.println("\n- Buona giornata");
-            }
-        }*/
 
         scanner.close();
     }
---
+++ src/org/lessons/java/ProgrammEventi.java
@@ -0,0 +1,77 @@
+package org.lessons.java;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ProgrammEventi {
+    private String titolo;
+    private List<Evento> eventi;
+
+    public ProgrammEventi(String titolo) {
+        checkStringha(titolo, "titolo");
+
+        this.titolo = titolo;
+        this.eventi = new ArrayList<>();
+    }
+
+    // Getters
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    // Setters
+
+    public void setTitolo(String titolo) {
+        checkStringha(titolo, "titolo");
+        this.titolo = titolo;
+    }
+
+    // Controlli
+
+    private void checkStringha(String stringa, String nomeStringa) throws IllegalArgumentException {
+        if (stringa == null || stringa.isBlank()) {
+            throw new IllegalArgumentException(nomeStringa + " è null o vuoto");
+        }
+    }
+
+    // Utilità
+
+    public void aggiungiEvento(Evento evento) {
+        this.eventi.add(evento);
+    }
+
+    public List cercaEventiDallaData(LocalDate data) {
+        return eventi
+                .stream()
+                .filter(d -> Objects.equals(d.getData(), data))
+                .toList();
+    }
+
+    public int numeroEventi() {
+        return eventi.size();
+    }
+
+    public void svuotaEventi() {
+        this.eventi.clear();
+    }
+
+    public String eventiOrdinatiPerData() {
+        return getTitolo() + "\n" +
+                eventi
+                .stream()
+                .sorted(Comparator.comparing(Evento::getData))
+                .map(Evento::toString)
+                .collect(Collectors.joining("\n"));
+    }
+
+    @Override
+    public String toString() {
+        return "titolo: " + titolo  + "\n" +
+                "Lista eventi: " + eventi;
+    }
+}
